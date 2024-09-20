a309cc3f3025e34b7ed333a578aa3bf59bd87ee0
JustinKeyes <137119242+JustinKeyes@users.noreply.github.com>
2024-09-19 23:11:04 -0400

    Adding calculator project to gir repo

---
+++ 2.6_305.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Calculator',
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _input = '';
+  String _result = '';
+
+  void _onButtonPressed(String value) {
+    setState(() {
+      if (value == '=') {
+        _calculateResult();
+      } else if (value == 'C') {
+        _input = '';
+        _result = '';
+      } else {
+        _input += value;
+      }
+    });
+  }
+
+  void _calculateResult() {
+    try {
+      final expression = Expression.parse(_input);
+      final evaluator = const ExpressionEvaluator();
+      var evalResult = evaluator.eval(expression, null);
+      _result = evalResult.toString();
+      _input += ' = $_result';
+    } catch (e) {
+      _result = 'Error';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Name\'s Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              alignment: Alignment.centerRight,
+              child: Text(
+                _input,
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              child: Text(
+                _result,
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('7'),
+                _buildButton('8'),
+                _buildButton('9'),
+                _buildButton('/'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('4'),
+                _buildButton('5'),
+                _buildButton('6'),
+                _buildButton('*'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('1'),
+                _buildButton('2'),
+                _buildButton('3'),
+                _buildButton('-'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButton('C'),
+                _buildButton('0'),
+                _buildButton('='),
+                _buildButton('+'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton(String value) {
+    return ElevatedButton(
+      onPressed: () => _onButtonPressed(value),
+      child: Text(value, style: TextStyle(fontSize: 24)),
+    );
+  }
+}
